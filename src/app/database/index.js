b2f14969b6988d5da64e162fed15375ce05c28b8
Omar Jafor Chowdhury <omarjaforchy@gmail.com>
2024-06-22 21:46:33 +0600

    Update index.js

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    const connectionUrl = 'mongodb+srv://omarjaforchy:CfWN2W5mNjwsKttu@cluster0.21hcnfr.mongodb.net/nextBlog';
+    const connectionUrl = '';
     mongoose
     .connect(connectionUrl)
     .then(() => console.log('Database Connection is Successfull'))
